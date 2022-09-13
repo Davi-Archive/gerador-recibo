8bdfb978c007023be95e6545205d8c99608c2bc1
davi38 <davi4alves@gmail.com>
2022-09-12 22:32:05 -0300

    feat: API pdf request done

--- next.config.js
+++ next.config.js
@@ -1,24 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  swcMinify: true,
-   async headers() {
-    return [
-      {
-        source: "/pdf",
-        headers: [
-          {
-            key: "Content-Type",
-            value: "application/pdf",
-          },
-          {
-            key: "Content-Length",
-            value: '10',
-          }
-        ],
-      },
-    ];
-  },
+  swcMinify: true
 };
 
 
--- pages/api/pdf.ts
+++ pages/api/pdf.ts
@@ -1,30 +1,36 @@
+import { link } from 'fs';
 import { NextApiRequest, NextApiResponse } from 'next';
 import puppeteer from 'puppeteer';
 
 
+var linkGenerator:string;
 
 async function printPDF() {
-    console.log("printando PDF")
     const browser = await puppeteer.launch({ headless: true });
     const page = await browser.newPage();
-    await page.goto("https://www.google.com/", { waitUntil: "networkidle0" });
+    await page.goto(linkGenerator, { waitUntil: "networkidle0" });
+    await page.emulateMediaType('screen')
     const pdf = await page.pdf({ format: 'A4' });
     await browser.close();
+    console.log(`link do pdf ( ${linkGenerator} )`);
     return pdf;
-}
-
-type Data = {
-
 }
 
 export default function handler(
     req: NextApiRequest,
     res: NextApiResponse<any>
 ) {
-    //#testPdf
-    /* return pdf */
-    printPDF().then((pdf) => {
-        console.log(pdf)
-        res.status(200).send(pdf);
-    });
+    if (req.method === 'POST') {
+        const { nome } = req.body;
+        console.log(req.body)
+        linkGenerator = `https://www.google.com/${nome}`
+
+        /* return pdf */
+        printPDF().then((pdf) => {
+            res.setHeader('Content-Type', 'application/pdf');
+            /* res.setHeader('Content-Disposition', 'attachment; filename=recibo.pdf'); */ //download PDF
+            res.send(pdf)
+        });
+    }
+
 }
